665e144c61c5fef7e9f453360351b6b562dc1553
Tom Mertz & Jonathan Lin <student@epicodus.com>
2015-03-12 15:32:14 -0700

    Debug intval error

--- src/WeekDayFinder.php
+++ src/WeekDayFinder.php
@@ -17,16 +17,45 @@
             //days of the week in order from today
             $weekdays = array("Thurday", "Friday", "Saturday", "Sunday", "Monday", "Tuesday", "Wednesday");
 
-            //the input array for day's date
-            $today = explode("/", $date);
+            //the input array for the date enter
+            //Ex ["03", "12", "2015"]
+            $input_date = explode("/", $date);
+            $s_input_date = $input_date[1];
+            $s_input_month = $input_date[0];
+            $s_input_year = $input_date[2];
+            //number of day's since start of year
+            $number_of_days = 0;
 
-            $day_today = intval($today[1]);
-            $month_today = intval($today[0]);
-            $year_today = intval($today[2]);
+            //information for today's date
+            $days_to_today = 71;
 
-            for($i = 1; $i < $today[0]; $i++)
+            echo "M " . $input_date[0] . " - ";
 
-            return "Thursday";
+            echo "D " . $input_date[1] . " - ";
+
+            //input month, day, year
+            $input_date = intval($s_input_date);
+            $input_month = intval($s_input_month);
+            $input_year = intval($s_input_year);
+
+            echo $input_date . " D - ";
+            echo $input_month . " M - ";
+            echo $input_year . " Y - ";
+
+            //add the days in the month since the beginning of this year to the total
+            for($i = 1; $i < $input_month; $i++) {
+                echo "$i - ";
+                $number_of_days += $days_in_month[$i];
+            }
+            //add the remaining days in the month
+            $number_of_days += $input_date;
+
+            echo $number_of_days;
+
+            $difference_of_days = abs($number_of_days - $days_to_today);
+
+
+            return $weekdays[$difference_of_days % 7];
 
         }
     }
