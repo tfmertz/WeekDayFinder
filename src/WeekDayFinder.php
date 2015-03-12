89d98e51a5c8a1b11117a47e13722aa1ba675a40
Tom Mertz & Jonathan Lin <student@epicodus.com>
2015-03-12 14:58:15 -0700

    Pass first test

--- src/WeekDayFinder.php
+++ src/WeekDayFinder.php
@@ -3,7 +3,30 @@
     class WeekDayFinder {
 
 
-        function findDay() {
+        function findDay($date) {
+
+            // $weekdays = array("Mon" => "Monday", "Tue" => "Tuesday", "Wed" => "Wednesday", "Thu" => "Thursday", "Fri" => "Friday", "Sat" => "Saturday", "Sun" => "Sunday");
+            //
+            // $date = new DateTime($date);
+            // $day = $date->format('D');
+            //
+            // return $weekdays[$day];
+
+            //each number cooresponds to
+            $days_in_month = array(1 => 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31);
+            //days of the week in order from today
+            $weekdays = array("Thurday", "Friday", "Saturday", "Sunday", "Monday", "Tuesday", "Wednesday");
+
+            //the input array for day's date
+            $today = explode("/", $date);
+
+            $day_today = intval($today[1]);
+            $month_today = intval($today[0]);
+            $year_today = intval($today[2]);
+
+            for($i = 1; $i < $today[0]; $i++)
+
+            return "Thursday";
 
         }
     }
